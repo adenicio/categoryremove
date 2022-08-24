0fc66dbf879871f2ee0e5d73c1d264a99dfdd0bb
Adenicio Batista Freire <adenicio@gmail.com>
2022-08-24 18:00:11 -0300

    Initial commit

---
+++ src/main/java/com/adenix/categoryremove/CategoryremoveApplication.java
@@ -0,0 +1,13 @@
+package com.adenix.categoryremove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryremoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryremoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adenix/categoryremove/CategoryremoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adenix.categoryremove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryremoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
